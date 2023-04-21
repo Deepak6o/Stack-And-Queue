97955d2b8cc0286fc2d11f1c8569f33886194329
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-04-21 18:43:14 +0530

    Create Delete middle element of a stack.cpp

---
+++ Delete middle element of a stack.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void solve(stack<int>&s, int count, int sizeOfStack)
+    {
+        if(count==sizeOfStack/2)
+        {
+            s.pop();
+            return;
+        }
+        int num=s.top();
+        s.pop();
+        solve(s,count+1,sizeOfStack);
+        s.push(num);
+    }
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.. 
+        int count =0;
+        solve(s,count,sizeOfStack);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
