e55bb379090ce68ef07acabc978da7d4c44aae28
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-05-20 17:00:50 +0530

    Create 19. Queue reversal.cpp

---
+++ 19. Queue reversal.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    // add code here.
+    stack<int> s;
+    while(!q.empty())
+    {
+        int temp=q.front();
+        q.pop();
+        s.push(temp);
+    }
+    while(!s.empty())
+    {
+        int temp=s.top();
+        s.pop();
+        q.push(temp);
+    }
+    return q;
+}
