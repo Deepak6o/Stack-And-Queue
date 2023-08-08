8f7d04f945ea947928f46d4b3bafd337a9104cb7
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-08-08 12:53:27 +0530

    Update Delete middle element of a stack.cpp

--- Delete middle element of a stack.cpp
+++ Delete middle element of a stack.cpp
@@ -7,27 +7,51 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
+// class Solution
+// {
+//     public:
+//     //Function to delete middle element of a stack.
+//     void solve(stack<int>&s, int count, int sizeOfStack)
+//     {
+//         if(count==sizeOfStack/2)
+//         {
+//             s.pop();
+//             return;
+//         }
+//         int num=s.top();
+//         s.pop();
+//         solve(s,count+1,sizeOfStack);
+//         s.push(num);
+//     }
+//     void deleteMid(stack<int>&s, int sizeOfStack)
+//     {
+//         // code here.. 
+//         int count =0;
+//         solve(s,count,sizeOfStack);
+//     }
+// };
 class Solution
 {
     public:
     //Function to delete middle element of a stack.
-    void solve(stack<int>&s, int count, int sizeOfStack)
-    {
-        if(count==sizeOfStack/2)
-        {
-            s.pop();
-            return;
-        }
-        int num=s.top();
-        s.pop();
-        solve(s,count+1,sizeOfStack);
-        s.push(num);
-    }
     void deleteMid(stack<int>&s, int sizeOfStack)
     {
         // code here.. 
-        int count =0;
-        solve(s,count,sizeOfStack);
+        stack<int> st;
+        int k=sizeOfStack/2;
+        for(int i=0;i<k;i++)
+        {
+            int x=s.top();
+            s.pop();
+            st.push(x);
+        }
+        s.pop();
+        while(!st.empty())
+        {
+            int x=st.top();
+            st.pop();
+            s.push(x);
+        }
     }
 };
 
